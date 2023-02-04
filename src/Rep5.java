19e19b8d4bfe6a6e87850b8f6a86792d32ea6eb1
Hiroki Teruya <e205726@ie.u-ryukyu.ac.jp>
2023-02-04 19:26:15 +0900

    step3_com

--- src/Rep5.java
+++ src/Rep5.java
@@ -1,12 +1,8 @@
 
 public class Rep5 {
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        } catch(NullPointerException e){
-            System.out.println("エラーが発生しました。" + e.getMessage());
-        }    
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }    
 
 }
